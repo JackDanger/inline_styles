f5d7f6e958c7b211b44ebbcb8c7f03157617059b
Jack Danger Canty <gitcommit@6brand.com>
2009-12-22 14:34:50 -0800

    starting to test

--- test/test_inline_styles.rb
+++ test/test_inline_styles.rb
@@ -1,7 +1,28 @@
-require 'helper'
+require File.dirname(__FILE__) + '/helper'
 
 class TestInlineStyles < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  CSS = <<-EOCSS
+div {display: block;}
+div small {font-size: 0.6em}
+small {font-size: 0.7em}
+img {border: none}
+div small img {border: 1px solid #000}
+EOCSS
+
+  HTML - <<-EOHTML
+<div> Welcome </div>
+<small> stay awhile! </small>
+<div>
+  <small>
+    <img src='i.png'>
+  </small>
+</small>
+EOHTML
+
+  context "Applying CSS to HTML" do
+    setup {  }
+    sh
   end
+
 end
