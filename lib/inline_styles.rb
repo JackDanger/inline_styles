8f6fad0ade364cd1eb6e2d74b30423d536e8096a
Jack Danger Canty <gitcommit@6brand.com>
2009-12-22 16:18:06 -0800

    working with merging styles/specificity

--- lib/inline_styles.rb
+++ lib/inline_styles.rb
@@ -15,12 +15,31 @@ module InlineStyles
 
       tree = Hpricot(html)
 
-      parser.each_rule_set do |rule_set|
-        rule_set.each_selector do |selector, declarations, specificity|
-          (tree/selector).each do |element|
-            element['style'] ||= ''
-            element['style'] += declarations.to_s
-          end
+      stable_sorter = 0
+      selectors = []
+
+      # extracting selectors via the API rather than
+      # just reaching in and grabbing @selectors
+      parser.each_selector do |selector, declarations, specificity|
+        selectors << [selector, declarations, specificity]
+      end
+
+      # stable-sort the selectors so that we get them sorted
+      # by specificity but also keeping their rough
+      # original order. This is how CSS selectors are applied
+      # in a browser
+      selectors.sort_by do |selector|
+        [selector.last, stable_sorter += 1]
+      end.each do |selector, declarations, spec|
+        # Find each element matching the given slector
+        (tree/selector).each do |element|
+          # Merge any previously-inlined style with the
+          # latest (higher specificity) one
+          element['style'] ||= ''
+          element['style'] = CssParser.merge(
+            CssParser::RuleSet.new('', element['style'], 1),
+            CssParser::RuleSet.new('', declarations, 2)
+          ).declarations_to_s
         end
       end
 
@@ -35,5 +54,14 @@ module InlineStyles
         gem 'hpricot'
         require 'hpricot'
       end
+  end
+
+  # taken from http://moserei.de/index.php/17/stable-array-sorting-in-ruby
+  class StableSortingArray < Array
+    def sort_by
+      n = 0
+      c = lambda {|x| n+= 1; [x, n] }
+      sort {|a, b| yield(c.call(a), c.call(b)) }
+    end
   end
 end
